4f45a1b5c53808489d34884e0c15e0632aa318fc
Dimava <dimava2@ya.ru>
2020-08-02 16:04:33 +0300

    add belts to 2nd toolbar

--- src/js/game/hud/parts/tools_toolbar.js
+++ src/js/game/hud/parts/tools_toolbar.js
@@ -2,11 +2,19 @@ import { MetaCutterBuilding } from "../../buildings/cutter";
 import { MetaEnergyGenerator } from "../../buildings/energy_generator";
 import { enumLayer } from "../../root";
 import { HUDBaseToolbar } from "./base_toolbar";
+import { MetaBeltBaseBuilding } from "../../buildings/belt_base";
+import { MetaSplitterBuilding } from "../../buildings/splitter";
+import { MetaUndergroundBeltBuilding } from "../../buildings/underground_belt";
+
 import { MetaAdvancedProcessorBuilding } from "../../buildings/advanced_processor";
 import { MetaToolbarSwapperBuilding } from "../../buildings/toolbar_swapper";
 
 export const supportedBuildings = [
     MetaToolbarSwapperBuilding,
+
+    MetaBeltBaseBuilding,
+    MetaSplitterBuilding,
+    MetaUndergroundBeltBuilding,
     
     MetaEnergyGenerator,
     MetaAdvancedProcessorBuilding,
