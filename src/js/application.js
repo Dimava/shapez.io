85d11d47a88305175cc5f429ea87ccfb4bd6420b
Dimava <dimava2@ya.ru>
2020-08-06 20:12:15 +0300

    inject KeybindingsState into SettingsState

--- src/js/application.js
+++ src/js/application.js
@@ -143,7 +143,7 @@ export class Application {
             MobileWarningState,
             MainMenuState,
             InGameState,
-            SettingsState,
+            // SettingsState,
             KeybindingsState,
             AboutState,
             ChangelogState,
