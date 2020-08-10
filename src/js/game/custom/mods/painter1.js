0ebf043a71bdadba585e49c4a5c65dc26fb4afc6
Dimava <dimava2@ya.ru>
2020-08-10 20:30:52 +0300

    make 1painter paint 3 layers cuz im evil

--- src/js/game/custom/mods/painter1.js
+++ src/js/game/custom/mods/painter1.js
@@ -25,7 +25,7 @@ function colorShape(shape, color) {
 
     let layers = shape.split(":").map(e => e.split(""));
     for (let i = 0; i < 4; i++) {
-        let charges = 4;
+        let charges = 3;
         for (let j = layers.length - 1; j >= 0; --j) {
             if (layers[j][2 * i] != "-") {
                 layers[j][2 * i + 1] = color;
